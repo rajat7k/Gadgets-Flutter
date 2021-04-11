b7b1223a464cbcc1365ce9e26beca0bbfb840ac4
Rajat kaushik <kaushikrajat777@gmail.com>
2021-04-11 13:13:04 +0530

    implement cart item count to show on home screen floating button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
+import 'package:pracise_app_2/core/store.dart';
+import 'package:pracise_app_2/models/cart_model.dart';
 import 'package:pracise_app_2/utlis/routes.dart';
 import 'package:pracise_app_2/widgets/home_widgets/home_drawer.dart';
 import 'package:pracise_app_2/widgets/home_widgets/home_header.dart';
 import 'package:pracise_app_2/widgets/home_widgets/product_list.dart';
-
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -35,16 +36,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, MyRoutes.cartRoute);
-        },
-        backgroundColor: Colors.teal,
-        child: Icon(
-          CupertinoIcons.cart,
-          color: Colors.white,
-        ),
+      floatingActionButton: VxBuilder(
+        mutations: {AddMutation, RemoveMutation},
+        builder: (ctx, _) => FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, MyRoutes.cartRoute);
+          },
+          backgroundColor: Colors.teal,
+          child: Icon(
+            CupertinoIcons.cart,
+            color: Colors.white,
+          ),
+        ).badge(
+            size: 23,
+            count: _cart.items.length,
+            color: Colors.red,
+            textStyle: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            )),
       ),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Theme.of(context).accentColor),
